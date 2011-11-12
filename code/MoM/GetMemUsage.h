e2d12e51af29b223a4595704a310002b33a0c94c
ides <vandenbosch.idesbald@gmail.com>
2011-11-12 05:07:45 -0500

    Maxime's peak memory reading function

---
+++ code/MoM/GetMemUsage.h
@@ -0,0 +1,33 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+    
+// written by Maxime Martinasso, november 2011
+
+static long parseLine(char* line)
+{
+    long i = strlen(line);
+    while (*line < '0' || *line > '9') line++;
+    line[i-3] = '\0';
+    i = atol(line);
+    return i;
+}
+    
+
+long MemoryUsageGetPeak()
+{ 
+    FILE* file = fopen("/proc/self/status", "r");
+    long result = 0;
+    char line[128];
+    
+    while (fgets(line, 128, file) != NULL)
+    {
+        if (strncmp(line, "VmPeak:", 6) == 0) 
+        {
+           result = parseLine(line);
+           break;
+        }
+    }
+    fclose(file);
+    return result*1024;
+}
