4d356d4f526fddf4b1b5882aa2ece51fe1fc43bd
Ides <vandenbosch.idesbald@gmail.com>
2012-10-23 22:52:12 +0200

    Updated install support

--- install.sh
+++ install.sh
@@ -57,84 +57,63 @@ if [ $DISTRIB_TYPE = "1" ]; then
 
 elif [ $DISTRIB_TYPE = "2" ]; then
     echo "What is the version of your Ubuntu distribution?"
-    echo "  (1) Ubuntu 10.04"
-    echo "  (2) Ubuntu 10.10"
-    echo "  (3) Ubuntu 11.04"
-    echo "  (4) Ubuntu 12.04"
+    echo "  (1) Ubuntu 10.10"
+    echo "  (2) Ubuntu 11.04"
+    echo "  (3) Ubuntu 12.04"
+    echo "  (4) Ubuntu 12.10"
     echo " "
     echo "Enter the correct number here: "
     read DISTRIB
 
     if [ $DISTRIB = "1" ]; then
-        echo " OK, running install script for Ubuntu 10.04: $DIR_INSTALL_SCRIPTS/UBUNTU_10.04_INSTALL.sh"
-        echo " read the file if you want more info about what will be installed..."
-        cd $DIR_INSTALL_SCRIPTS
-        ./UBUNTU_10.04_INSTALL.sh
-        cd ..
-    elif [ $DISTRIB = "2" ]; then
         echo " OK, running install script for Ubuntu 10.10: $DIR_INSTALL_SCRIPTS/UBUNTU_10.10_INSTALL.sh"
         echo " read the file if you want more info about what will be installed..."
         cd $DIR_INSTALL_SCRIPTS
         ./UBUNTU_10.10_INSTALL.sh
         cd ..
-    elif [ $DISTRIB = "3" ]; then
+    elif [ $DISTRIB = "2" ]; then
         echo " OK, running install script for Ubuntu 11.04: $DIR_INSTALL_SCRIPTS/UBUNTU_11.04_INSTALL.sh"
         echo " read the file if you want more info about what will be installed..."
         cd $DIR_INSTALL_SCRIPTS
         ./UBUNTU_11.04_INSTALL.sh
         cd ..
-    elif [ $DISTRIB = "4" ]; then
+    elif [ $DISTRIB = "3" ]; then
         echo " OK, running install script for Ubuntu 12.04: $DIR_INSTALL_SCRIPTS/UBUNTU_12.04_INSTALL.sh"
         echo " read the file if you want more info about what will be installed..."
         cd $DIR_INSTALL_SCRIPTS
         ./UBUNTU_12.04_INSTALL.sh
         cd ..
+    elif [ $DISTRIB = "4" ]; then
+        echo " OK, running install script for Ubuntu 12.10: $DIR_INSTALL_SCRIPTS/UBUNTU_12.10_INSTALL.sh"
+        echo " read the file if you want more info about what will be installed..."
+        cd $DIR_INSTALL_SCRIPTS
+        ./UBUNTU_12.10_INSTALL.sh
+        cd ..
     fi
 
 elif [ $DISTRIB_TYPE = "3" ]; then
     echo "What is the version of your OpenSuse distribution?"
-    echo "  (1) OpenSuse 11.0, 64 bits"
-    echo "  (2) OpenSuse 11.1, 64 bits"
-    echo "  (3) OpenSuse 11.2, 64 bits"
-    echo "  (4) OpenSuse 11.3, 64 bits"
-    echo "  (5) OpenSuse 11.4, 64 bits"
-    echo "  (6) OpenSuse 12.1, 64 bits"
+    echo "  (1) OpenSuse 11.3, 64 bits"
+    echo "  (2) OpenSuse 11.4, 64 bits"
+    echo "  (3) OpenSuse 12.1, 64 bits"
     echo " "
     echo "Enter the correct number here: "
     read DISTRIB
 
 
     if [ $DISTRIB = "1" ]; then
-        echo " OK, running install script for OpenSuse 11.0: $DIR_INSTALL_SCRIPTS/OPENSUSE_11.0_64BITS_INSTALL.sh"
-        echo " read the file if you want more info about what will be installed..."
-        cd $DIR_INSTALL_SCRIPTS
-        ./OPENSUSE_11.0_64BITS_INSTALL.sh
-        cd ..
-    elif [ $DISTRIB = "2" ]; then
-        echo " OK, running install script for OpenSuse 11.1: $DIR_INSTALL_SCRIPTS/OPENSUSE_11.1_64BITS_INSTALL.sh"
-        echo " read the file if you want more info about what will be installed..."
-        cd $DIR_INSTALL_SCRIPTS
-        ./OPENSUSE_11.1_64BITS_INSTALL.sh
-        cd ..
-    elif [ $DISTRIB = "3" ]; then
-        echo " OK, running install script for OpenSuse 11.2: $DIR_INSTALL_SCRIPTS/OPENSUSE_11.2_64BITS_INSTALL.sh"
-        echo " read the file if you want more info about what will be installed..."
-        cd $DIR_INSTALL_SCRIPTS
-        ./OPENSUSE_11.2_64BITS_INSTALL.sh
-        cd ..
-    elif [ $DISTRIB = "4" ]; then
         echo " OK, running install script for OpenSuse 11.3: $DIR_INSTALL_SCRIPTS/OPENSUSE_11.3_64BITS_INSTALL.sh"
         echo " read the file if you want more info about what will be installed..."
         cd $DIR_INSTALL_SCRIPTS
         ./OPENSUSE_11.3_64BITS_INSTALL.sh
         cd ..
-    elif [ $DISTRIB = "5" ]; then
+    elif [ $DISTRIB = "2" ]; then
         echo " OK, running install script for OpenSuse 11.4: $DIR_INSTALL_SCRIPTS/OPENSUSE_11.4_64BITS_INSTALL.sh"
         echo " read the file if you want more info about what will be installed..."
         cd $DIR_INSTALL_SCRIPTS
         ./OPENSUSE_11.4_64BITS_INSTALL.sh
         cd ..
-    elif [ $DISTRIB = "6" ]; then
+    elif [ $DISTRIB = "3" ]; then
         echo " OK, running install script for OpenSuse 12.1: $DIR_INSTALL_SCRIPTS/OPENSUSE_12.1_64BITS_INSTALL.sh"
         echo " read the file if you want more info about what will be installed..."
         cd $DIR_INSTALL_SCRIPTS
