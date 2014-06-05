38a6bf9ed694ced4a14b8649893bf7e36eb72799
ides <vandenbosch.idesbald@gmail.com>
2014-06-05 12:49:26 -0300

    Small changes in new file code/setup_MLFMA_poles.py

--- code/setup_MLFMA_poles.py
+++ code/setup_MLFMA_poles.py
@@ -1,5 +1,5 @@
 from mpi4py import MPI
-import sys, os, argparse, copy
+import sys, os, argparse
 try:
     import cPickle
 except ImportError:
@@ -12,9 +12,9 @@ from ReadWriteBlitzArray import writeScalarToDisk, writeASCIIBlitzArrayToDisk
 def L_computation(k, a, NB_DIGITS):
     """this function computes the number of expansion poles given the wavenumber k and the sidelength a"""
     L_tmp1 = a*real(k)*sqrt(3) + NB_DIGITS * (a*real(k)*sqrt(3))**(1./3.)
-    L_tmp2 = a*real(k)*sqrt(3) + NB_DIGITS * log10(a*real(k)*sqrt(3) + pi)
+    L_tmp2 = a*real(k)*sqrt(3) + NB_DIGITS * log10(a*real(k)*sqrt(3) + pi) # Guillaume SYLVAND: p. 129
     L_tmp3 = a*real(k)*sqrt(3) + 1 * log(a*real(k)*sqrt(3) + pi)
-    L_tmp4 = a*real(k)*sqrt(3) + 1.8 * NB_DIGITS**(2.0/3.0) * (a*real(k)*sqrt(3))**(1.0/3.0)
+    L_tmp4 = a*real(k)*sqrt(3) + 1.8 * NB_DIGITS**(2.0/3.0) * (a*real(k)*sqrt(3))**(1.0/3.0) # Song, Chew, "Fast and Efficient..."
     #my_id = MPI.COMM_WORLD.Get_rank()
     #if (my_id==0):
         #print "L_tmp1 =", L_tmp1, ", L_tmp2 =", L_tmp2, ", L_tmp3 =", L_tmp3, ", L_tmp4 =", L_tmp4
