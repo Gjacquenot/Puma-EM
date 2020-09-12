eddb9435169d2ca43b8a0a1883a6fcfe613fb62e
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2020-09-12 13:28:37 +0200

    Added parentheses for pritn commit

--- code/FMM.py
+++ code/FMM.py
@@ -10,7 +10,7 @@ def L_computation(k, a, NB_DIGITS):
     L_tmp4 = a*real(k)*sqrt(3) + 1.8 * NB_DIGITS**(2.0/3.0) * (a*real(k)*sqrt(3))**(1.0/3.0)
     #my_id = MPI.COMM_WORLD.Get_rank()
     #if (my_id==0):
-        #print "L_tmp1 =", L_tmp1, ", L_tmp2 =", L_tmp2, ", L_tmp3 =", L_tmp3, ", L_tmp4 =", L_tmp4
+        #print("L_tmp1 =", L_tmp1, ", L_tmp2 =", L_tmp2, ", L_tmp3 =", L_tmp3, ", L_tmp4 =", L_tmp4)
     L2 = where(L_tmp2<5., 5, int(ceil( L_tmp2 )))
     #return L2
     return int(floor( L_tmp2 ))
