a8125f5157b25e8994eb7fb85b1bd671dd338a63
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2020-09-12 13:28:37 +0200

    Added parentheses for pritn commit

--- code/compute_SAI_precond_MLFMA.py
+++ code/compute_SAI_precond_MLFMA.py
@@ -27,11 +27,11 @@ def compute_SAIpreconditioner(tmpDirName, C, chunkNumber_to_cubesNumbers, cubeNu
         if 'G2C' in elem:
             LIB_G2C = elem.split('=')[1].split()[0]
     if (my_id == 0):
-        print "SAI preconditioner computation..."
+        print("SAI preconditioner computation...")
     MPI.COMM_WORLD.Barrier()
     Mg_CSR(my_id, processNumber_to_ChunksNumbers, chunkNumber_to_cubesNumbers, cubeNumber_to_chunkNumber, ELEM_TYPE, Z_TMP_ELEM_TYPE, LIB_G2C, pathToReadFrom, pathToSaveTo)
     MPI.COMM_WORLD.Barrier()
-    CPU_time_Mg_computation = time.clock() - CPU_t0 
+    CPU_time_Mg_computation = time.clock() - CPU_t0
     Wall_time_Mg_computation = time.time() - Wall_t0
     # assembling of near interactions matrix
     pathToReadFrom = os.path.join(tmpDirName, 'Z_tmp')
@@ -50,8 +50,8 @@ def compute_SAI(params_simu, simuDirName):
     variables = cPickle.load(file)
     file.close()
     #if my_id==0:
-        ##print variables['chunkNumber_to_cubesNumbers']
-        #print variables['cubeNumber_to_chunkNumber']
+        ##print(variables['chunkNumber_to_cubesNumbers'])
+        #print(variables['cubeNumber_to_chunkNumber'])
     # writing the chunk numbers (per process)
     chunkNumbers = variables['processNumber_to_ChunksNumbers'][my_id]
     writeASCIIBlitzArrayToDisk(array(chunkNumbers).astype('i'), os.path.join(pathToSaveTo, 'chunkNumbers.txt'))
@@ -59,16 +59,16 @@ def compute_SAI(params_simu, simuDirName):
     for chunk in chunkNumbers:
         writeASCIIBlitzArrayToDisk(array(variables['chunkNumber_to_cubesNumbers'][chunk]).astype('i'), os.path.join(tmpDirName, 'Mg_LeftFrob', "chunk" + str(chunk) + 'cubesNumbers.txt'))
     # writing the chunk numbers (cube-dependent)
-    writeASCIIBlitzArrayToDisk(array(variables['cubeNumber_to_chunkNumber']).astype('i'), os.path.join(pathToSaveTo, 'cubeNumber_to_chunkNumber.txt')) 
+    writeASCIIBlitzArrayToDisk(array(variables['cubeNumber_to_chunkNumber']).astype('i'), os.path.join(pathToSaveTo, 'cubeNumber_to_chunkNumber.txt'))
     # SAI precond computation
     Wall_time_Mg_computation, CPU_time_Mg_computation = compute_SAIpreconditioner(tmpDirName, variables['C'], variables['chunkNumber_to_cubesNumbers'], variables['cubeNumber_to_chunkNumber'], variables['chunkNumber_to_processNumber'], variables['processNumber_to_ChunksNumbers'], params_simu.MAX_BLOCK_SIZE)
     variables['Wall_time_Mg_computation'] = Wall_time_Mg_computation
     variables['CPU_time_Mg_computation'] = CPU_time_Mg_computation
     if (my_id == 0) and (params_simu.VERBOSE == 1):
-        print variables['CPU_time_Z_near_computation'], "CPU time (seconds) for constructing Z_CFIE_near"
-        print variables['Wall_time_Z_near_computation'], "Wall time (seconds) for constructing Z_CFIE_near"
-        print variables['CPU_time_Mg_computation'], "CPU time (seconds) for constructing SAI precond"
-        print variables['Wall_time_Mg_computation'], "Wall time (seconds) for constructing SAI precond"
+        print(variables['CPU_time_Z_near_computation'], "CPU time (seconds) for constructing Z_CFIE_near")
+        print(variables['Wall_time_Z_near_computation'], "Wall time (seconds) for constructing Z_CFIE_near")
+        print(variables['CPU_time_Mg_computation'], "CPU time (seconds) for constructing SAI precond")
+        print(variables['Wall_time_Mg_computation'], "Wall time (seconds) for constructing SAI precond")
     file = open(os.path.join(tmpDirName, 'pickle', 'variables.txt'), 'wb')
     cPickle.dump(variables, file)
     file.close()
@@ -92,6 +92,6 @@ if __name__=='__main__':
     if (params_simu.MONOSTATIC_RCS==1) or (params_simu.MONOSTATIC_SAR==1) or (params_simu.BISTATIC==1):
         compute_SAI(params_simu, simuDirName)
     else:
-        print "you should select monostatic RCS or monostatic SAR or bistatic computation, or a combination of these computations. Check the simulation settings."
+        print("you should select monostatic RCS or monostatic SAR or bistatic computation, or a combination of these computations. Check the simulation settings.")
         sys.exit(1)
     #MPI.Finalize()
